f379d4e3031f1501245209f88ac1c34da43db600
marble <msm.mirdamad@gmail.com>
2025-01-23 00:21:26 +0330

    complete tree.h file

--- src-ui/tree.h
+++ src-ui/tree.h
@@ -1,8 +1,27 @@
 #ifndef TREE_H
 #define TREE_H
 
-class Tree {
+#include "node.h"
+#include <QDir>
 
+class Tree {
+private:
+    Node *root;
+
+public:
+    Tree();
+
+    void setRoot(Node *root);
+
+    Node* getRoot();
+
+    bool fillTheTree(QDir, QStringList);
+    void textToWords(QString, QString);
+    void insertWord(string, QString);
+    void editInputs(string, string, string, QStringList&, QStringList&, QStringList&);
+    QStringList searchFileNames(string, string, string, QDir);
+    QStringList searchWord(string);
+    void deleteWord(string);
 };
 
 #endif // TREE_H
