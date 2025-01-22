9f9da9991f5df742253a5b5986716c886cf355dc
marble <msm.mirdamad@gmail.com>
2025-01-23 00:32:45 +0330

    complete search.h file

--- src-ui/search.h
+++ src-ui/search.h
@@ -2,17 +2,31 @@
 #define SEARCH_H
 
 #include <QMainWindow>
+#include <QDir>
+
+using namespace std;
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Search; }
 QT_END_NAMESPACE
 
-class Search : public QMainWindow {
-    Q_OBJECT
-
+class Search : public QMainWindow { Q_OBJECT
 public:
     Search(QWidget *parent = nullptr);
     ~Search();
+
+    void connections();
+    bool editFiles(QDir&, QStringList&, string);
+    bool openFilesReadOnly(QDir, QString, QString&);
+    bool openFilesWriteOnly(QDir, QString, QString);
+    void toLowerCase(string&);
+    QStringList fileNamesByCondition(QStringList, QStringList, QStringList, QStringList, QStringList, QDir);
+    QStringList findCommonElements(QStringList, QStringList);
+    QStringList removeCommonElements(QStringList, QStringList, QStringList, QStringList, QDir);
+    void printFileNames(QStringList);
+
+private slots:
+    void onSearchButtonClicked();
 
 private:
     Ui::Search *ui;
